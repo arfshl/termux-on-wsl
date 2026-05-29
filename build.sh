0a9d13e041fd5aecff0bacd0d234446559826a36
Alif Putra Winata <98870352+arfshl@users.noreply.github.com>
2026-05-29 20:16:02 +0700

    test build 8

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ case "$ARCH" in
         exit 1
         ;;
 esac
-ech0 "RELEASE=$RELEASE" >> "$GITHUB_OUTPUT"
+echo "RELEASE=$RELEASE" >> "$GITHUB_OUTPUT"
 echo "ARCH=$ARCH" >> "$GITHUB_OUTPUT"
 
 # start build
